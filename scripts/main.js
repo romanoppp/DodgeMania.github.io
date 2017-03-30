b6adc3f0a4d70773ba66203f684bbc2bcf4fc6f1
romanoppp <titiroupou@gmail.com>
2017-03-31 01:33:12 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -154,3 +154,61 @@ function bougerBalle(){
   dirY=-dirY;
  }
 }
+
+/*MECHANT*/
+var jeu = document.querySelector("#game"),
+    mechant = document.querySelector("#mechant"), //car en query on metles selecteur css //
+    posX1 = 4,
+    posY1 = 4,
+    dirX1 = 20,
+    dirY1 = 10,
+    vitesse = 40;
+var orientationX = 0.3; // Valeur de déplacement (0.3 "px" par défaut)
+var orientationY = 0.3; // Valeur de déplacement (0.3 "px" par défaut)
+var timing = 1000/150; // x images par seconde ("150" par défaut ici)
+
+init();
+
+function init() {
+ jeu.style.width = largeurJeu + "px";
+ jeu.style.height = hauteurJeu + "px";
+ mechant.style.left = posX1 + "px";
+ mechant.style.right = posY1 + "px";
+
+
+ setInterval(
+  function () {
+   bougerBalle1();
+
+
+  },
+  vitesse
+ );
+}
+
+function bougerBalle1() {
+ if(gamestart == 1){
+  posX1 = posX1 + dirX1;
+  posY1 = posY1 + dirY1;
+  mechant.style.left = posX1 + "px";
+  mechant.style.top = posY1 + "px";
+
+  if (posX1 >= (largeurJeu - mechant.offsetWidth) || posX1 <= 0) {
+   dirX1 = -dirX1;
+  }
+  if (posY1 >= (hauteurJeu - mechant.offsetHeight) || posY1 <= 0) {
+   dirY1 = -dirY1;
+  }
+ }
+}
+
+/*************collision*****************/
+var posXBalle25 = posXBalle + 25px;
+var posYBalle25 = posYBalle + 25px;
+function collision(){
+    if(posX1 == posXBalle || posY1 == posYBalle){
+        function gamestart == 0;
+    }else if(posX1 == posXBalle25  || posY1 == posYBalle25){
+        function gamestart ==0;
+}
+
